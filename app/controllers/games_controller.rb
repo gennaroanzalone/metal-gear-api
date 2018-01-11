c771d7d3e241695ec932809253ac42cac8b9a8af
Gennaro <g.anzalone12.com>
2018-01-11 14:30:46 -0800

    remove json reponse code portion to be added to Response  module

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,34 @@
 class GamesController < ApplicationController
 
+  def index
+    @games = Game.all
+    json_response(@games)
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    json_response(@game)
+  end
+
+  def create
+    @game = Game.create(game_params)
+    json_response(@game)
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+  end
 
   private
-  def json_response(object)
-    render json: object, status: :ok
+
+  def game_params
+    params.permit(:name, :release, :description, :story, :gameplay, :development)
   end
+
 end
